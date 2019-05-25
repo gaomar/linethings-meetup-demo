e921843c444cd91ddb53395ff1f245d3b1b116ac
h-takauma <gaomar@gaomar-mbp-2.local>
2019-05-25 21:59:12 +0900

    初回コミット

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,96 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app id="inspire">
+    <v-container>
+      <v-layout
+        text-xs-center
+        wrap
+      >
+        <v-flex mb-4>
+          <h1 class="display-2 font-weight-bold mb-3">
+            認証画面
+          </h1>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-form>
+            <v-container>
+              <v-layout row wrap>
+                <v-text-field
+                  v-model="name"
+                  @keyup.enter="say"
+                  @keypress="setCanSubmit"
+                  outline
+                  label="認証コードを入力してください"
+                ></v-text-field>
+              </v-layout>
+            </v-container>
+          </v-form>  
+        </v-flex>
+
+        <v-flex xs12>
+          {{this.bleStatus}}
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    data () {
+      return {
+        USER_SERVICE_UUID: 'b38c905b-9968-45b8-b392-04e62b8a7842',
+        bleConnect: false,
+        canSubmit: false,
+        bleStatus: '',
+        user: {
+          image: '',
+          userId: ''
+        }        
+      }
+    },
+    methods: {
+      setCanSubmit () {
+        this.canSubmit = true
+      },
+      // BLEが接続できる状態になるまでリトライ
+      liffCheckAvailablityAndDo: async function (callbackIfAvailable) {
+        try {
+          const isAvailable = await liff.bluetooth.getAvailability();
+          if (isAvailable) {
+            callbackIfAvailable()
+          } else {
+            // リトライ
+            this.bleStatus = `Bluetoothをオンにしてください。`
+            setTimeout(() => this.liffCheckAvailablityAndDo(callbackIfAvailable), 10000)
+          }
+        } catch (error) {
+          alert('Bluetoothをオンにしてください。')
+        }
+      },
+      // サービスとキャラクタリスティックにアクセス
+      liffRequestDevice: async function () {
+        const device = await liff.bluetooth.requestDevice()
+        await device.gatt.connect()
+        const service = await device.gatt.getPrimaryService(this.USER_SERVICE_UUID)
+        window.ledCharacteristic = await service.getCharacteristic(this.LED_CHARACTERISTIC_UUID)
+        this.bleConnect = true
+        this.bleStatus = `デバイスに接続しました。`
+      },
+      initializeLiff: async function(){
+        await liff.initPlugins(['bluetooth']);
+        this.liffCheckAvailablityAndDo(() => this.liffRequestDevice())
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+        // プロフゲット
+        const profile = await liff.getProfile()
+        this.user.image = profile.pictureUrl
+        this.user.userId = profile.userId
+      }
+    },
+    mounted: function () {
+      liff.init(
+          () => this.initializeLiff()
+      )
+    }
+  }
+</script>
