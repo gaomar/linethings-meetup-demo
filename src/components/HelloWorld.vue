b145014ba4eafd5d1ad338e40f9cc26bb4cde7fe
h-takauma <gaomar@gaomar-mbp-5.local>
2019-05-26 18:27:06 +0900

    UUID更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,7 +39,7 @@
   export default {
     data () {
       return {
-        USER_SERVICE_UUID: 'b38c905b-9968-45b8-b392-04e62b8a7842',
+        USER_SERVICE_UUID: 'e4a93c43-f923-4c1f-9091-6172ec39ec81',
         LED_CHARACTERISTIC_UUID: 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B',
         bleConnect: false,
         canSubmit: false,
